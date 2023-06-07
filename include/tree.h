2544a99d8b64d54471fe26e8d78be4b265680cb9
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-06-07 23:53:56 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,10 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <algorithm>
+#include  <iostream>
+#include <vector>
+
 class Tree {
  private:
     struct Node {
