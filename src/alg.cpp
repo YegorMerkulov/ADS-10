55a9801cb200e2d5a3b630d77d7ca87441dcdf25
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-06-07 23:57:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,7 @@
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
-   std::vector<std::vector<char>> permutes = tree.getAllPermutes();
+  std::vector<std::vector<char>> permutes = tree.getAllPermutes();
     if (permutes.size() >= n) {
         return permutes[n - 1];
     }
